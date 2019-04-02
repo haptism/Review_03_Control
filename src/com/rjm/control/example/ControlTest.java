e19a709069c093c239231e1f2a0158cfd4fffd69
{haptism} <haptism007@gmail.com>
2019-04-02 17:40:22 +0900

    Initial commit

---
+++ src/com/rjm/control/example/ControlTest.java
@@ -0,0 +1,25 @@
+package com.rjm.control.example;
+
+public class ControlTest {
+	public void test1() {
+	}
+
+	public void test2() {
+	}
+
+	public void test3() {
+	}
+
+	public void test4() {
+	}
+
+	public void inputTest() {
+	}
+
+	public void test6() {
+	}
+
+	public void test7() {
+	}
+
+}
---
+++ src/com/rjm/control/example/ControlTestMain.java
@@ -0,0 +1,10 @@
+package com.rjm.control.example;
+
+public class ControlTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
